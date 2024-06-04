25e951c8956fa40f5c9aeabe290364d8262c8b84
Luke Pilon <luke.pilon@gmail.com>
2024-06-04 18:43:46 -0500

    initial commit - spring boot initialization

---
+++ src/main/java/com/york/aboutreferralservice/AboutReferralServiceApplication.java
@@ -0,0 +1,13 @@
+package com.york.aboutreferralservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AboutReferralServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AboutReferralServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/york/aboutreferralservice/AboutReferralServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.york.aboutreferralservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AboutReferralServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
